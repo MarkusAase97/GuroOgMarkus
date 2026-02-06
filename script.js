eb6f456740dda18a1fc8fe96c7237c616efac06d
MarkusAase97 <168534198+MarkusAase97@users.noreply.github.com>
2026-02-06 11:10:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+// PROGRAM
 document.getElementById('toggleProgram').addEventListener('click', function () {
-const content = document.getElementById('programContent');
-if (content.classList.contains('hidden')) {
-content.classList.remove('hidden');
-this.textContent = 'Skjul program';
-} else {
-content.classList.add('hidden');
-this.textContent = 'Vis program';
-}
+  const content = document.getElementById('programContent');
+  if (content.classList.contains('hidden')) {
+    content.classList.remove('hidden');
+    this.textContent = 'Skjul program';
+  } else {
+    content.classList.add('hidden');
+    this.textContent = 'Vis program';
+  }
+});
+
+// HJELP
+document.getElementById('toggleHjelp').addEventListener('click', function () {
+  const content = document.getElementById('hjelpContent');
+  if (content.classList.contains('hidden')) {
+    content.classList.remove('hidden');
+    this.textContent = 'Skjul';
+  } else {
+    content.classList.add('hidden');
+    this.textContent = 'Ønsker du å hjelpe?';
+  }
+});
+
+// OVERNATTING
+document.getElementById('toggleOvernatting').addEventListener('click', function () {
+  const content = document.getElementById('overnattingContent');
+  if (content.classList.contains('hidden')) {
+    content.classList.remove('hidden');
+    this.textContent = 'Skjul overnatting';
+  } else {
+    content.classList.add('hidden');
+    this.textContent = 'Trenger du overnatting?';
+  }
 });
